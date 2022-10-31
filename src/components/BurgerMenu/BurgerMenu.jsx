1bd777f5ae79d0e6e7d0b5ae8c19b1588ec28db8
Denis <kip22ops@gmail.com>
2022-10-31 14:17:20 +0300

    Fixed/SearchMobile-category

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,16 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { CartBlock } from '../CartTotal/CartBlock/CartBlock'
 import { DiCodrops } from 'react-icons/di'
-import { AiOutlineSearch, AiOutlineHome, AiOutlineMenu } from 'react-icons/ai'
-import { MdFavoriteBorder, MdNewReleases, MdFiberNew } from 'react-icons/md'
 import { BiCategoryAlt } from 'react-icons/bi'
 import { ImCancelCircle } from 'react-icons/im'
-import { SiPopos } from 'react-icons/si'
-import { sortedCart } from '../../Redux/Cart/cartSlice'
-import { useDispatch } from 'react-redux'
-import { sortedGenres } from '../../Redux/Cart/cartSlice'
-import { addDiscription } from '../../Redux/Cart/cartSlice'
 import NavCategoryMobile from '../NavCategory/NavCategoryMobile'
 
 
@@ -37,37 +28,12 @@ const BurgerMenu = ({isOpenNav, setOpenMenu, isOpenMenu, clearSorted, setOpenNav
           <div className={styleCategories.categories} onClick={() => setOpenMenu(!isOpenMenu)}><ImCancelCircle size={30}/></div>
           </div>
 
-          {/* Items */}
-          <ul className='flex flex-col justify-center items-center  text-3xl  gap-[20px] cursor-pointer'>
-					<Link to='/'>
-						<div onClick={clearSorted} className={styleCategories.categories}>
-							<AiOutlineHome />
-							<li>Home</li>
-						</div>
-					</Link>
-					
-					<div className={styleCategories.categories}>
-						<MdFiberNew />
-						<li onClick={() => sortedFunc('&sort-by=release-date')}>New</li>
-					</div>
-					<div className={styleCategories.categories}>
-						<SiPopos />
-						<li onClick={() => sortedFunc('&sort-by=popularity')}>Popular</li>
-					</div>
-					<div className={styleCategories.categories}>
-						<MdNewReleases />
-						<li>Promotions and Offers</li>
-					</div>
-				</ul>
+
 
           {/* Categories  */}
 
-        <div className='flex justify-center items-center mt-20 text-3xl gap-[10px] mb-5 '>
-						<BiCategoryAlt />
 
-						<div onClick={() => setOpenNav(!isOpenNav)}>Categories:</div>
-					</div>
-            <NavCategoryMobile setOpenMenu={setOpenMenu} isOpenMenu={isOpenMenu}/>
+            <NavCategoryMobile setOpenMenu={setOpenMenu} sortedFunc={sortedFunc} clearSorted={clearSorted} isOpenMenu={isOpenMenu}/>
 				</div>
 			</div>
 		</div>
