7ac6309430244c3a42eb3c09750d287ade4f06d6
Denis <kip22ops@gmail.com>
2022-11-03 23:41:04 +0300

    FixedSwiper

--- src/components/PhotoSwiper/PhotoSwiper.jsx
+++ src/components/PhotoSwiper/PhotoSwiper.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navigation, Thumbs } from 'swiper'
+import {  EffectCoverflow, Navigation, Thumbs } from 'swiper'
 import {Swiper,SwiperSlide} from 'swiper/react'
 import {GiCancel} from 'react-icons/gi'
 import 'swiper/css'
@@ -11,20 +11,20 @@ import 'swiper/css/thumbs'
 const PhotoSwiper = ({link, setVisibleSwiper}) => {
 	return (
 		<div>
-			<div className='fixed w-full h-full  z-20 top-0 left-0 bg-black/50 rounded-xl '>
+			<div className='fixed w-full h-full  z-20 top-0 left-0 bg-black/50 rounded-xl flex justify-center items-center '>
 <GiCancel onClick={() => setVisibleSwiper(false)} size={30} className='fixed top-[10%] right-[10%] cursor-pointer'/>
-			<div className='fixed sm:w-[70%] w-[100%]  z-30 sm:top-[15%] top-[30%] sm:left-[15%]'>
+			<div className='fixed   z-30 lg:max-w-[1000px] md:max-w-[800px] sm:max-w-[600px] max-w-[400px] '>
         <Swiper
         loop={true}
-        spaceBetween={10}
         navigation={true}
-        modules={[Navigation, Thumbs]}
+        modules={[Navigation, Thumbs, EffectCoverflow]}
+        effect='coverflow'
         grabCursor={true}
         >
           
             {link.map(item => (
-              <SwiperSlide>
-              <img src={item.image} className='rounded-xl' />
+              <SwiperSlide >
+              <img src={item.image} className='rounded-xl ' />
               </SwiperSlide>
             ))}
          
